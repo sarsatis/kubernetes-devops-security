b965ddedca9e668a4010b9759e6ee82f90bf8128
sarsatis <sarthak8055@gmail.com>
2023-10-29 20:31:46 +0000

    sa

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 scan_result=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .|0|.message -r )
-scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .|0|.score )
+scan_message=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].message -r )
+scan_score=$(curl -sSX POST --data-binary @"k8s_deployment_service.yaml" https://v2.kubesec.io/scan | jq .[0].score )
 
 if [[ "${scan_score}" -ge 5 ]]; then
     echo "Score is $scan_score"
